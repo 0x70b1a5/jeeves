44e2cbd46afe4c7ece5a041543d0a9cb1a29bb8b
Tobias Merkle <tgamerkle@gmail.com>
2024-03-14 16:46:31 -0400

    ui2

---
+++ ui/src/App.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect, useCallback } from "react";
+import KinodeClientApi from "@kinode/client-api";
+
+const BASE_URL = import.meta.env.BASE_URL;
+if (window.our) window.our.process = BASE_URL?.replace("/", "");
+
+const PROXY_TARGET = `${(import.meta.env.VITE_NODE_URL || "http://localhost:8080")}${BASE_URL}`;
+
+// This env also has BASE_URL which should match the process + package name
+const WEBSOCKET_URL = import.meta.env.DEV
+  ? `${PROXY_TARGET.replace('http', 'ws')}`
+  : undefined;
+
+function App() {
+  const [nodeConnected, setNodeConnected] = useState(true);
+  const [api, setApi] = useState<KinodeClientApi | undefined>();
+
+  useEffect(() => {
+    // Connect to the Kinode via websocket
+    console.log('WEBSOCKET URL', WEBSOCKET_URL)
+    if (window.our?.node && window.our?.process) {
+      const api = new KinodeClientApi({
+        uri: WEBSOCKET_URL,
+        nodeId: window.our.node,
+        processId: window.our.process,
+        onOpen: (_event, _api) => {
+          console.log("Connected to Kinode");
+        },
+        onMessage: (json, _api) => {
+          console.log('WEBSOCKET MESSAGE', json)
+          try {
+            const data = JSON.parse(json);
+            console.log("WebSocket received message", data);
+            const [messageType] = Object.keys(data);
+            if (!messageType) return;
+            console.log({ messageType })
+          } catch (error) {
+            console.error("Error parsing WebSocket message", error);
+          }
+        },
+      });
+
+      setApi(api);
+    } else {
+      setNodeConnected(false);
+    }
+  }, []);
+
+  return (
+    <div className='w-screen h-screen flex flex-col place-items-center place-content-center'>
+      <h1>Jeeves</h1>
+    </div>
+  );
+}
+
+export default App;
